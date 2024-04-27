e690d87f62c87ad34639a05fcfa9b6879f583d3e
Tomasz Śmiałek <tsmialek.1@gmail.com>
2024-04-27 13:55:54 +0200

    Changes:
    - application is working but only for one audio effect.

--- Lab3 - drumkit/script.js
+++ Lab3 - drumkit/script.js
@@ -6,15 +6,44 @@ const soundObject = {
 
 const recordBtn = document.querySelector('.record');
 const stopBtn = document.querySelector('.stop');
+const playBtn = document.querySelector('.play');
+const sound = soundObject.a;
 let isRecording = false;
+let startTime;
+const soundArr = [];
 
 const toggleRecording = () => {
   recordBtn.classList.toggle('hidden');
   stopBtn.classList.toggle('hidden');
+  isRecording = !isRecording;
+  startTime = Date.now();
+  console.log(`recording: ${isRecording}`);
 };
 
+recordBtn.addEventListener('click', () => {
+  soundArr.length = 0;
+  toggleRecording();
+});
+
+stopBtn.addEventListener('click', () => {
+  toggleRecording();
+  console.log(`sound array: ${soundArr}`);
+});
+
 document.addEventListener('keydown', (e) => {
-  const sound = soundObject[e.key];
   sound.currentTime = 0;
   sound.play();
+  if (isRecording) {
+    console.log('recorded sound');
+    soundArr.push(Date.now() - startTime);
+  }
+});
+
+playBtn.addEventListener('click', () => {
+  soundArr.forEach((delay) => {
+    setTimeout(() => {
+      sound.currentTime = 0;
+      sound.play();
+    }, delay);
+  });
 });
