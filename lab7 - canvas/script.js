d77757698b068cbbc4d4c30e84cac0773a1b07ff
tsmialek <tsmialek.1@gmail.com>
2024-06-16 12:28:45 +0200

    feat(canvas): 1st stage almost complete. ToDo: Add start / stop button and particles number choice.

--- lab7 - canvas/script.js
+++ lab7 - canvas/script.js
@@ -2,43 +2,120 @@
 
 const canvas = document.querySelector('#animationContainer');
 const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-const radius = 10;
-const circles = [];
-const speed = 2;
+// colors
+const TEXT = '#d1d7fd';
+const LINE = '#a809a9';
+const BACKGROUND = '#d1d7fd';
 
-class Circle {
-  constructor(x, y, vx, vy, radius) {
+let particlesArr;
+
+// mouse setup
+const mouse = {
+  x: null,
+  y: null,
+  radius: (canvas.height / 60) * (canvas.width / 60),
+};
+
+window.addEventListener('mousemove', (e) => {
+  mouse.x = e.x;
+  mouse.y = e.y;
+});
+
+// particle
+class Particle {
+  constructor(x, y, directionX, directionY, size, color) {
     this.x = x;
     this.y = y;
-    this.vx = vx;
-    this.vy = vy;
-    this.radius = radius;
+    this.directionX = directionX;
+    this.directionY = directionY;
+    this.size = size;
+    this.color = color;
   }
 
   draw() {
     ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
+    ctx.fillStyle = this.color;
     ctx.fill();
     ctx.closePath();
   }
 
   update() {
-    // update position and detect collision with walls
+    // bouncing off walls
+    if (this.x > canvas.width || this.x < 0) {
+      this.directionX = -this.directionX;
+    }
+
+    if (this.y > canvas.height || this.y < 0) {
+      this.directionY = -this.directionY;
+    }
+
+    // collision detectio
+    let dx = mouse.x - this.x;
+    let dy = mouse.y - this.y;
+    let distance = Math.sqrt(dx * dx + dy * dy);
+    if (distance < mouse.radius + this.size) {
+      // ToDo:
+      // checking which side of the mouse the particle is
+    }
+
+    this.x += this.directionX;
+    this.y += this.directionY;
+
+    this.draw();
   }
 }
 
-const drawCircles = (n) => {
+// create particles
+const init = (n, color, speed) => {
+  particlesArr = [];
+
   for (let i = 0; i < n; i++) {
-    const x = Math.random() * canvas.width;
-    const y = Math.random() * canvas.height;
+    let size = Math.random() * 5 + 1;
+    let x = Math.random() * (innerWidth - size * 2 - size * 2) + size * 2;
+    let y = Math.random() * (innerHeight - size * 2 - size * 2) + size * 2;
 
-    const vx = (Math.random() - 0.5) * speed;
-    const vy = (Math.random() - 0.5) * speed;
+    let directionX = Math.random() * speed - speed / 2;
+    let directionY = Math.random() * speed - speed / 2;
 
-    const circle = new Circle(x, y, vx, vy);
-    circles.push(circle);
+    particlesArr.push(new Particle(x, y, directionX, directionY, size, color));
   }
 };
 
-window.addEventListener('load', drawCircles(10));
+const animate = () => {
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, innerWidth, innerHeight);
+
+  for (let i = 0; i < particlesArr.length; i++) {
+    particlesArr[i].update();
+  }
+  connect(100, TEXT, 0.01);
+};
+
+const connect = (treshold, color, lineWidth) => {
+  particlesArr.forEach((a) => {
+    particlesArr.forEach((b) => {
+      let distance = (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y);
+
+      if (distance < treshold * treshold) {
+        ctx.strokeStyle = color;
+        ctx.lineWidth = lineWidth;
+        ctx.moveTo(a.x, a.y);
+        ctx.lineTo(b.x, b.y);
+        ctx.stroke();
+      }
+    });
+  });
+};
+window.addEventListener('resize', () => {
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+  init(100, TEXT);
+});
+
+// initialize
+init(80, TEXT, 3);
+animate();
